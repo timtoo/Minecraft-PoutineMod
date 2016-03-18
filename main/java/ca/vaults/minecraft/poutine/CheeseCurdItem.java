0038f3ae244b2ef18a964f842317937692b49baa
Tim Middleton <tim@vaults.ca>
2016-03-18 12:24:40 -0400

    it builds, but names and graphics don't work

--- main/java/ca/vaults/minecraft/poutine/CheeseCurdItem.java
+++ main/java/ca/vaults/minecraft/poutine/CheeseCurdItem.java
@@ -1,21 +1,25 @@
 package ca.vaults.minecraft.poutine;
 
-import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+//import net.minecraft.creativetab.CreativeTabs;
+//import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 
 public class CheeseCurdItem extends ItemFood {
 	
-	private String name = "cheeseCurdItem";
+	private final String name = "cheeseCurdItem";
 	
 	public CheeseCurdItem(int heal, float saturation, boolean wolfMeat) {
 		super(heal, saturation, wolfMeat);
 		this.setMaxStackSize(64);
-		this.setUnlocalizedName("cheese_curd");
-		this.setTextureName("poutinemod:cheese_curd2");
+		this.setUnlocalizedName(Poutine.MODID + "_" + name);
+		//this.setTextureName("poutinemod:cheese_curd2");
 		GameRegistry.registerItem(this, name);
 		
+	}
+	
+	public String getName() {
+		return name;
 	}
 
 }
--- main/java/ca/vaults/minecraft/poutine/GravyItem.java
+++ main/java/ca/vaults/minecraft/poutine/GravyItem.java
@@ -1,21 +1,22 @@
 package ca.vaults.minecraft.poutine;
 
-import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemSoup;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class GravyItem extends ItemSoup {
 	
-	private String name = "gravyItem";
+	private final String name = "gravyItem";
 	
 	public GravyItem(int heal) {
 		super(heal);
-		setUnlocalizedName("gravy");
-		setTextureName("poutinemod:mushroom_gravy");
+		setUnlocalizedName(Poutine.MODID + "_" + name);
+		//setTextureName("poutinemod:mushroom_gravy");
 		GameRegistry.registerItem(this, name);
 		
+	}
+	
+	public String getName() {
+		return name;
 	}
 
 }
